6a222b442a6b0879fdb136720ce14fbea2bc4ae5
dangtft <haidang29072000@gmail.com>
2024-05-12 09:30:08 +0700

    AddMVC

---
+++ PetShops/PetShopMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PetShopMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- PetShops/PetShops/obj/Debug/net8.0/PetShops.AssemblyInfo.cs
+++ PetShops/PetShops/obj/Debug/net8.0/PetShops.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetShops")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e1f09b60e112cda569b470770dca6b13dda4582")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetShops")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetShops")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
